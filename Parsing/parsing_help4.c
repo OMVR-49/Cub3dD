32e6a551f6777662916f3713e7a740d2e0c9d112
Omar Jebbari <ojebbari@e1r5p9.1337.ma>
2024-03-27 14:35:58 +0000

    RGB Fixed

--- Parsing/parsing_help4.c
+++ Parsing/parsing_help4.c
@@ -6,7 +6,7 @@
 /*   By: ojebbari <ojebbari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 22:05:45 by sacharai          #+#    #+#             */
-/*   Updated: 2024/03/27 14:03:41 by ojebbari         ###   ########.fr       */
+/*   Updated: 2024/03/27 14:34:45 by ojebbari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,7 +90,6 @@ int	calculate_rgb(char **tmprgb)
 {
 	int	rgb;
 
-	rgb = ft_atoi(tmprgb[0]) * 65536
-		+ ft_atoi(tmprgb[1]) * 256 + ft_atoi(tmprgb[2]);
+	rgb = ft_atoi(tmprgb[0]) << 24 | ft_atoi(tmprgb[1]) << 16 | ft_atoi(tmprgb[2]) << 8 | 255;
 	return (rgb);
 }
